bf6c9b9b370fb00ad814d5efcf69d03eae6bbe2f
kaneda0911 <51037928+kaneda0911@users.noreply.github.com>
2019-05-30 20:02:07 +0900

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,19 @@
+$(function() {
+  'use strict';
+
+ $('#todos').on('click', 'update_todo', function() {
+
+ });
+
+});
+
+// 前回レッスンで久保インストが最後書いたコード？
+// function append(){
+//  $('main').append('<ul class="container"></ul>')
+//  $('#button').click(function(){
+//  var value = $('#input').val();
+//  $('.container').append('<li class="list">${value}</li>');
+//  $('#input').val('');
+// });
+// return;
+// }
